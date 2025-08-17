fcaec1c20fa1b5a35252e1c8131c7d20da308e70
senCode23 <1508787861@qq.com>
2025-08-17 17:45:22 +0800

    update

--- 第二天/vite-project/src/Header.tsx
+++ 第二天/vite-project/src/Header.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header style={{ backgroundColor: 'grey', textAlign: 'center',color:'white' }}>我是Header</header>
+      <header 
+      style={{ backgroundColor: 'grey', textAlign: 'center',color:'white' }}>我是Header 
+      </header>
     );
   }
 }
